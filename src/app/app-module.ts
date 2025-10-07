eca33f8daa7589efd5a637ae8f5ee93556897d71
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-10-06 21:08:58 -0300

    :sparkles: feat: Add Header component closes #7

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -6,10 +6,12 @@ import { App } from './app';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { Header } from './components/shared/header/header';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    Header
   ],
   imports: [
     BrowserModule,
