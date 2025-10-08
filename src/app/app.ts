6441d5e48eb83bbeec9205a69693796c40865420
felip-SN <felipenascimentooudri@gmail.com>
2025-10-08 12:28:37 -0300

    feat: Add DaySelector component closes #13

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,17 +7,5 @@ import type { Day } from './components/shared/day-selector/day-selector';
   standalone: false,
 })
 export class App {
-    availableDays: Day[] = [
-    { id: '2025-10-01', date: '01', dayOfWeek: 'qua' },
-    { id: '2025-10-02', date: '02', dayOfWeek: 'qui' },
-    { id: '2025-10-03', date: '03', dayOfWeek: 'sex' },
-    { id: '2025-10-04', date: '04', dayOfWeek: 'sab' },
-    { id: '2025-10-05', date: '05', dayOfWeek: 'dom' },
-  ];
 
-  selectedDayId: string | number = '2025-10-03';
-
-  selectDay(id: string | number) {
-    this.selectedDayId = id;
-  }
 }
