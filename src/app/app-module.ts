417db66b9d36f02686d320d8adb1a3fb2053096a
Isaque Barbosa <isaquebatista400@gmail.com>
2025-10-07 21:44:27 -0300

    :art: Add design system

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -7,6 +7,9 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { Header } from './components/shared/header/header';
+import { TitleHighligh } from './components/shared/title-highligh/title-highligh';
+import { provideHttpClient } from '@angular/common/http';
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
   declarations: [
@@ -18,13 +21,15 @@ import { Header } from './components/shared/header/header';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
+    MatIconModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: environment.production,
       registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
   providers: [
-    provideBrowserGlobalErrorListeners()
+    provideBrowserGlobalErrorListeners(),
+    provideHttpClient()
   ],
   bootstrap: [App]
 })
---
+++ src/app/services/iconService/icon-registry.ts
@@ -0,0 +1,37 @@
+// Em src/app/services/icon-registry.service.ts
+import { inject, Injectable } from '@angular/core';
+import { MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IconRegistryService {
+  private iconPath = 'assets/icons/';
+  private matIconRegistry = inject(MatIconRegistry);
+  private domSanitizer = inject(DomSanitizer);
+
+  registerIcons(): void {
+    this.addIcon('ads-icon', 'ads-icon.svg');
+    this.addIcon('alert', 'alert.svg');
+    this.addIcon('arrow-down', 'arrow-down.svg');
+    this.addIcon('calendar', 'calendar.svg');
+    this.addIcon('check', 'check.svg');
+    this.addIcon('close', 'close.svg');
+    this.addIcon('document', 'document.svg');
+    this.addIcon('list', 'list.svg');
+    this.addIcon('icon-fatec', 'icon-fatec.svg');
+    this.addIcon('menu', 'menu.svg');
+    this.addIcon('minimize', 'minimize.svg');
+    this.addIcon('notification', 'notification.svg');
+    this.addIcon('settings', 'settings.svg');
+    this.addIcon('user', 'user.svg');
+  }
+
+  private addIcon(iconName: string, fileName: string): void {
+    this.matIconRegistry.addSvgIcon(
+      iconName,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(this.iconPath + fileName)
+    );
+  }
+}
