2982ff71f33a8aa7c2139a974e8ca3511ecd60f1
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-10-09 20:55:48 -0300

    :construction: Modal template created

--- src/app/components/shared/confirmation-modal/confirmation-modal.ts
+++ src/app/components/shared/confirmation-modal/confirmation-modal.ts
@@ -1,4 +1,6 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import type { TemplateRef, WritableSignal } from '@angular/core';
+import { Component, EventEmitter, inject, Input, Output, signal } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-confirmation-modal',
@@ -7,6 +9,10 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   styleUrl: './confirmation-modal.css'
 })
 export class ConfirmationModal {
+ 
+  private modalService = inject(NgbModal);
+  closeResult: WritableSignal<string> = signal('');
+
 
   @Input({required : true}) title!: string;
   @Input({required : true}) message!: string;
@@ -15,6 +21,9 @@ export class ConfirmationModal {
 
   @Output() modalConfirm = new EventEmitter<void>();
   @Output() modalCancel = new EventEmitter<void>();
-
+  
+ 		openVerticallyCentered(content: TemplateRef<any>) {
+		this.modalService.open(content, { centered: true });
+	}
 
 }
