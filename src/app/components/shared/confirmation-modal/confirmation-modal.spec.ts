b78eb2209b98135eb8bf373675b2c2c67f2fd7f8
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-10-10 16:33:45 -0300

    :lipstick: fix: Confirmation Modal component appearence

--- src/app/components/shared/confirmation-modal/confirmation-modal.spec.ts
+++ src/app/components/shared/confirmation-modal/confirmation-modal.spec.ts
@@ -1,4 +1,4 @@
-import type { ComponentFixture} from '@angular/core/testing';
+import type { ComponentFixture } from '@angular/core/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ConfirmationModal } from './confirmation-modal';
@@ -9,9 +9,8 @@ describe('ConfirmationModal', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ConfirmationModal]
-    })
-    .compileComponents();
+      declarations: [ConfirmationModal],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmationModal);
     component = fixture.componentInstance;
