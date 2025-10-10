d65c70e387c5f2c18d13dcc51c6c958875c51828
Isaque Batista Barbosa <160277060+IsaqueBatist@users.noreply.github.com>
2025-10-10 08:57:01 -0300

    Remove unused import of ComponentFixture

--- src/app/components/class-info-card/class-info-card.spec.ts
+++ src/app/components/class-info-card/class-info-card.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import type { ComponentFixture } from '@angular/core/testing';
 
 import { ClassInfoCard } from './class-info-card';
