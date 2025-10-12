76ee0021b8ec861664156afa742bd50881679441
Isaque Barbosa <isaquebatista400@gmail.com>
2025-10-12 11:41:35 -0300

    :wrench: add ngrx store dependence

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,8 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, isDevMode } from '@angular/core';
 import { IconRegistryService } from './services/iconService/icon-registry';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 @Component({
   selector: 'app-root',
