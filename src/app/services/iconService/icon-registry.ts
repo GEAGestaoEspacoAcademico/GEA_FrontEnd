6ed874be03618b184ad52e1dadb1cf4f617b2da9
Isaque Barbosa <isaquebatista400@gmail.com>
2025-10-14 17:45:38 -0300

    :wrench: add arrow-back icon

--- src/app/services/iconService/icon-registry.ts
+++ src/app/services/iconService/icon-registry.ts
@@ -27,7 +27,8 @@ export class IconRegistryService {
     this.addIcon('notification-fill', 'notification-fill.svg')
     this.addIcon('settings', 'settings.svg');
     this.addIcon('user', 'user.svg');
-    this.addIcon('logout', 'logout.svg')
+    this.addIcon('logout', 'logout.svg');
+    this.addIcon('arrow-back', 'arrow-back.svg')
   }
 
   private addIcon(iconName: string, fileName: string): void {
