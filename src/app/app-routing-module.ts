812d4b92863f8882aa578aec1c925255f7df1bc5
Isaque Batista Barbosa <160277060+IsaqueBatist@users.noreply.github.com>
2025-10-15 09:20:01 -0300

    :wrench: Add route for editing aulas and update redirect path

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -18,8 +18,9 @@ const routes: Routes = [
       { path: 'agenda', component: Agenda },
       { path: 'aulas', component: Aulas },
       { path: 'configuracoes', component: Configuracoes },
+      {path: 'aulas/alterar/:id', component: EditarAula},
       { path: 'notificacoes', component: Notificacoes },
-      { path: '', redirectTo: '/aulas', pathMatch: 'full' }
+      { path: '', redirectTo: '/login', pathMatch: 'full' }
     ]
   },
   {
