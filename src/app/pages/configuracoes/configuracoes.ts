7e12c7ac311a5d0669e2f875cf08247b68c79405
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-10-21 21:33:46 -0300

    :construction: implemented confirmation modal on configuracoes page

--- src/app/pages/configuracoes/configuracoes.ts
+++ src/app/pages/configuracoes/configuracoes.ts
@@ -1,11 +1,12 @@
 import type { OnInit } from '@angular/core';
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import type { Teacher } from '../../models/teacher.model';
 import { AuthActions } from '../../store/auth/auth.actions';
 import { Store } from '@ngrx/store';
 import type { User } from '../../models/user.model';
 import type { Observable } from 'rxjs';
 import { selectCurrentUser } from '../../store/auth/auth.selectors';
+import type { ConfirmationModal } from '../../components/shared/confirmation-modal/confirmation-modal';
 
 @Component({
   selector: 'app-configuracoes',
@@ -13,43 +14,51 @@ import { selectCurrentUser } from '../../store/auth/auth.selectors';
   templateUrl: './configuracoes.html',
   styleUrl: './configuracoes.css',
 })
-export class Configuracoes implements OnInit{
+export class Configuracoes implements OnInit {
   ngOnInit(): void {
     this.user$ = this.store.select(selectCurrentUser);
   }
   private store = inject(Store);
   user$!: Observable<User | null>;
 
+  @ViewChild('ConfirmationModal')
+  confirmModal!: ConfirmationModal;
+
+  openModal() {
+    this.confirmModal.open();
+  }
+
+  closeModal() {
+    this.confirmModal.onModalCancel();
+  }
+
   logout() {
     this.store.dispatch(AuthActions.logout());
   }
+
   mockTeacher: Teacher = {
-    nome: "Prof. Dr. Lorem Ipsum",
-    disciplinas: [
-      "Cálculo 1",
-      "Cálculo 2",
-      "Álgebra Linear",
-    ],
+    nome: 'Prof. Dr. Lorem Ipsum',
+    disciplinas: ['Cálculo 1', 'Cálculo 2', 'Álgebra Linear'],
     cursos: [
       {
-        semestre: "1º semestre",
-        curso: "Engenharia Mecatrônica",
-        disciplina: "Cálculo 1",
+        semestre: '1º semestre',
+        curso: 'Engenharia Mecatrônica',
+        disciplina: 'Cálculo 1',
       },
       {
-        semestre: "2º semestre",
-        curso: "Análise e Desenvolvimento de Sistemas (ADS)",
-        disciplina: "Cálculo 1",
+        semestre: '2º semestre',
+        curso: 'Análise e Desenvolvimento de Sistemas (ADS)',
+        disciplina: 'Cálculo 1',
       },
       {
-        semestre: "2º semestre",
-        curso: "Engenharia de Software",
-        disciplina: "Álgebra Linear",
+        semestre: '2º semestre',
+        curso: 'Engenharia de Software',
+        disciplina: 'Álgebra Linear',
       },
       {
-        semestre: "3º semestre",
-        curso: "Engenharia Mecatrônica",
-        disciplina: "Cálculo 2",
+        semestre: '3º semestre',
+        curso: 'Engenharia Mecatrônica',
+        disciplina: 'Cálculo 2',
       },
     ],
   };
