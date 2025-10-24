4e352de464f2d58a01bb62d5f5437a807fdbe0b1
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-10-23 23:02:12 -0300

    :sparkles: feat: implemented toasts through notification service closes #85

---
+++ src/app/services/notificacoes/notification-service.ts
@@ -0,0 +1,30 @@
+import { inject, Injectable } from '@angular/core';
+import type { MatSnackBarHorizontalPosition, MatSnackBarConfig } from '@angular/material/snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NotificationService {
+  private snackBar = inject(MatSnackBar);
+
+  horizontalPosition: MatSnackBarHorizontalPosition = 'center';
+
+  private createConfig(panelClass: string, duration: number = 30000000): MatSnackBarConfig {
+    return {
+      duration,
+      horizontalPosition: this.horizontalPosition,
+      panelClass: [panelClass],
+    };
+  }
+
+  showSuccess(message: string): void {
+    const config = this.createConfig('toast-success');
+    this.snackBar.open(message, 'Fechar', config);
+  }
+
+  showError(message: string): void {
+    const config = this.createConfig('toast-error');
+    this.snackBar.open(message, 'Fechar', config);
+  }
+}
