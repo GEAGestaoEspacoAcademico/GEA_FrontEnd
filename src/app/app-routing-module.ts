1b3a91a2d7052267b67d0e7c47571599360c46df
pie.tro <pietromaia28@gmail.com>
2025-10-25 16:54:30 -0300

    :hammer: improv/change on app.routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -16,6 +16,7 @@ const routes: Routes = [
     path: '',
     component: MainLayout,
     canActivate: [RoleGuard],
+    data: { roles: ["PROFESSOR"] },
     children: [
       { path: 'agenda', component: Agenda },
       { path: 'aulas', component: Aulas },
