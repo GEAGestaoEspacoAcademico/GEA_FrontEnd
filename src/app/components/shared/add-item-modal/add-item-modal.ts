bce6657da40e08edc312514e98c5c60d71fce254
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-11-06 15:51:47 -0300

    :construction: Criado: componente addItemModal

---
+++ src/app/components/shared/add-item-modal/add-item-modal.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-item-modal',
+  standalone: false,
+  templateUrl: './add-item-modal.html',
+  styleUrl: './add-item-modal.css'
+})
+export class AddItemModal {
+
+}
