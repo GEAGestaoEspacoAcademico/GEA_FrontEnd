7fc2aa4226f1331f14d2de1837f53a3cffefe37c
Isaque Barbosa <isaquebatista400@gmail.com>
2025-11-07 16:40:50 -0300

    :sparkles: feat(janelasHorario): Adiciona servico para gerenciar janelasHorarios

---
+++ src/app/services/janelas-horario/janelas-horario.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import type { JanelaHorario } from '../../models/janelasHorario.model';
+import type { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JanelasHorarioService {  
+  private http = inject(HttpClient);
+  private baseUrl = environment.apiUrl + "/janelas-horario";
+
+
+  public getJanelasHorario(): Observable<JanelaHorario[]>{
+    return this.http.get<JanelaHorario[]>(this.baseUrl)
+  }
+
+}
