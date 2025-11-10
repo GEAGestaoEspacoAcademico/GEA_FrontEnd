9a5fed24d6633be6666f3dff0cb35c5b4c2b399b
YungCrock <enzorodrigues164@gmail.com>
2025-11-10 15:55:58 -0300

    :recycle: improvement:HeaderComponent refatorado para suportar layout dinamico closes #120

--- src/app/components/shared/header/header.ts
+++ src/app/components/shared/header/header.ts
@@ -1,8 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { map, type Observable } from 'rxjs';
+import { type Observable } from 'rxjs';
 import { HeaderTitleService } from '../../../services/header-title/header-title.service';
-import { selectCurrentUser, selectUserCargo } from '../../../store/auth/auth.selectors';
+import { selectUserCargo } from '../../../store/auth/auth.selectors';
 import { AuthActions } from '../../../store/auth/auth.actions';
 
 /**
@@ -21,20 +21,7 @@ export class Header {
   private store = inject(Store);
   private headerTitleService = inject(HeaderTitleService);
 
-  public user$ = this.store.select(selectCurrentUser);
-
-  public userCargo$: Observable<string | undefined> = this.store.select(selectUserCargo);
-
-  public isProfessor$: Observable<boolean> = this.userCargo$.pipe(
-    map(cargo => (cargo ?? '').toUpperCase() === 'PROFESSOR')
-  );
-
-  public isAdminOrSecretaria$: Observable<boolean> = this.userCargo$.pipe(
-    map(cargo => {
-      const c = (cargo ?? '').toUpperCase();
-      return c === 'AD' || c === 'SECRETARIA';
-    })
-  );
+  public cargo$: Observable<string | undefined> = this.store.select(selectUserCargo);
 
   public title$: Observable<string> = this.headerTitleService.title$;
 
