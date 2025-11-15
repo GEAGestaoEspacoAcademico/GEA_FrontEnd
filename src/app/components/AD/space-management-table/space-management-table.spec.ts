9648990cbcf575f4283d68bf81f8b3350184cf89
Isaque Batista Barbosa <160277060+IsaqueBatist@users.noreply.github.com>
2025-11-15 09:41:14 -0300

    :hammer: Refactor imports in space-management-table.spec.ts

--- src/app/components/AD/space-management-table/space-management-table.spec.ts
+++ src/app/components/AD/space-management-table/space-management-table.spec.ts
@@ -1,5 +1,5 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import type { ComponentFixture} from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { SpaceManagementTable } from './space-management-table';
 
 describe('SpaceManagementTable', () => {
