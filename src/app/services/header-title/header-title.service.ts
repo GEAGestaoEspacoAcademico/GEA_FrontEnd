f8568a9c15c0e5ede5396dab56fb0abd3c1e58a7
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-11-19 15:41:46 -0300

    :construction: add: variavel showBackButton e seus metodos de controle

--- src/app/services/header-title/header-title.service.ts
+++ src/app/services/header-title/header-title.service.ts
@@ -3,11 +3,21 @@ import type { Observable } from 'rxjs';
 import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HeaderTitleService {
   private titleSubject = new BehaviorSubject<string>('Painel');
   public title$: Observable<string> = this.titleSubject.asObservable();
+  public showBackButton = new BehaviorSubject<boolean>(false);
+  public showBackButton$: Observable<boolean> = this.showBackButton.asObservable();
+
+  showBack(): void {
+    this.showBackButton.next(true);
+  }
+
+  hideBack(): void {
+    this.showBackButton.next(false);
+  }
 
   setTitle(title: string): void {
     this.titleSubject.next(title);
