e963f74c2c583651671d5c78c67fdb611dd04db6
Isaque Barbosa <isaquebatista400@gmail.com>
2025-11-21 12:59:25 -0300

    :hammer: Arrumando conflitos

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -66,13 +66,10 @@ import { SpaceManagement } from './pages/space-management/space-management';
 // AD Components
 import { SpaceManagementTable } from './components/AD/space-management-table/space-management-table';
 import { Home } from './pages/AD/home/home';
-<<<<<<< HEAD
 import { FuncionarioTable } from './components/secretaria/funcionario-table/funcionario-table';
 
-=======
 import { Secretariahome } from './pages/secretaria/secretariahome/secretariahome';
 import { CreateResourceModal } from './components/modals/create-resource-modal/create-resource-modal';
->>>>>>> origin/develop
 
 @NgModule({
   declarations: [
@@ -110,12 +107,9 @@ import { CreateResourceModal } from './components/modals/create-resource-modal/c
     SpaceManagementTable,
     SpaceRegistrationForm,
     Home,
-<<<<<<< HEAD
-    FuncionarioTable
-=======
+    FuncionarioTable,
     Secretariahome,
     CreateResourceModal
->>>>>>> origin/develop
   ],
   imports: [
     BrowserModule,
--- src/app/components/AD/space-management-table/space-management-table.ts
+++ src/app/components/AD/space-management-table/space-management-table.ts
@@ -38,10 +38,7 @@ export class SpaceManagementTable {
       this.pageChange.emit(page);
     }
   }
-<<<<<<< HEAD
-=======
 
->>>>>>> origin/develop
   onNovoSoftware() {
     this.clickNovoSoftware.emit();
   }
