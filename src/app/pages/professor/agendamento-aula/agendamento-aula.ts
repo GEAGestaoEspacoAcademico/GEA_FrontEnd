debfd7d4ab78b230fe42e2eb0a6478c827db122d
Nicolas Augusto <nick.nogueira02@gmail.com>
2025-11-21 15:59:00 -0300

    :construction: add: mat-chip-grid para equipamentos e softwares

--- src/app/pages/professor/agendamento-aula/agendamento-aula.ts
+++ src/app/pages/professor/agendamento-aula/agendamento-aula.ts
@@ -2,6 +2,7 @@ import type { OnInit } from '@angular/core';
 import { Component, inject } from '@angular/core';
 import { DisciplinaService } from '../../../services/disciplina/disciplina.service';
 import type { Disciplina } from '../../../models/disciplina.model';
+import { JanelasHorarioService } from '../../../services/janelas-horario/janelas-horario.service';
 
 @Component({
   selector: 'app-agendamento-aula',
@@ -11,6 +12,7 @@ import type { Disciplina } from '../../../models/disciplina.model';
 })
 export class AgendamentoAula implements OnInit {
   private disciplinaService = inject(DisciplinaService);
+  private horariosService = inject(JanelasHorarioService);
 
   disciplinas: Disciplina[] = [];
 
@@ -20,4 +22,45 @@ export class AgendamentoAula implements OnInit {
       error: (err) => console.error(err),
     });
   }
+
+  equipamentos: Equipamento[] = [];
+  softwares: string[] = [];
+
+  adicionarEquipamento(nomeValue: string, qtdValue: string): void {
+    const nome = (nomeValue || '').trim();
+    const qtd = parseInt(qtdValue, 10);
+
+    if (nome && qtd > 0) {
+      this.equipamentos.push({ nome: nome, qtd: qtd });
+    }
+  }
+
+  removerEquipamento(equipamento: Equipamento): void {
+    const index = this.equipamentos.indexOf(equipamento);
+
+    if (index >= 0) {
+      this.equipamentos.splice(index, 1);
+    }
+  }
+
+  adicionarSoftware(nomeValue: string): void {
+    const nome = (nomeValue || '').trim();
+
+    if (nome && !this.softwares.map((s) => s.toLowerCase()).includes(nome.toLowerCase())) {
+      this.softwares.push(nome);
+    }
+  }
+
+  removerSoftware(software: string): void {
+    const index = this.softwares.indexOf(software);
+
+    if (index >= 0) {
+      this.softwares.splice(index, 1);
+    }
+  }
+}
+
+export interface Equipamento {
+  nome: string;
+  qtd: number;
 }
