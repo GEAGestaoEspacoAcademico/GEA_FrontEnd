a22f46be360aa24aace5fe2293ae72d6435f61b9
Lucas Morikawa <lucasmorikawa23@gmail.com>
2025-11-28 23:30:10 -0300

    Pagina Vizualizcao Esp Academico: issue218

---
+++ src/app/pages/shared/espacos-academicos/espacos-academicos.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EspacosAcademicos } from './espacos-academicos';
+
+describe('EspacosAcademicos', () => {
+  let component: EspacosAcademicos;
+  let fixture: ComponentFixture<EspacosAcademicos>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EspacosAcademicos]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(EspacosAcademicos);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
