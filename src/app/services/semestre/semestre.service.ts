4053459ab7e1a47b185acd6b678e859c5e20f7aa
bibiritriz <ntenhodoritosmasdepende@gmail.com>
2025-12-07 14:45:06 -0300

    feat: adiciona service de semestre e seu model

---
+++ src/app/services/semestre/semestre.service.ts
@@ -0,0 +1,17 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import type { Observable } from 'rxjs';
+import type { Semestre } from '../../models/semestre.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SemestreService {
+  private readonly http = inject(HttpClient);
+  private readonly baseUrl = environment.apiUrl + '/semestres';
+
+  public listar(): Observable<Semestre[]> {
+    return this.http.get<Semestre[]>(this.baseUrl);
+  }
+}
